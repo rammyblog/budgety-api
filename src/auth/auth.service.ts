95b0ac707e72522a3afe8253527a235c6f1f3f4d
Babatunde Onasanya <onasanyatunde67@gmail.com>
2023-06-03 23:15:57 +0100

    Setup prisma

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  async register() {
+    return 'register';
+  }
+
+  async login() {
+    return 'login';
+  }
+}
