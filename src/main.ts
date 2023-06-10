b1cc9bb6717941901b62f5524f3c816a07ee1e58
Babatunde Onasanya <onasanyatunde67@gmail.com>
2023-06-10 23:27:03 +0100

    works on fly now

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Queue } from 'bull';
 import { ExpressAdapter } from '@bull-board/express';
 import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  // const options = new DocumentBuilder()
+  //   .setTitle('Cats example')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addTag('cats')
+  //   .addBearerAuth()
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('swagger', app, document);
   // Setup BullBoard UI
   const budgetsQueue = app.get<Queue>('BullQueue_budgets');
   const serverAdapter = new ExpressAdapter();
@@ -22,6 +33,7 @@ async function bootstrap() {
   });
   serverAdapter.setBasePath('/admin/queue');
   app.use('/admin/queue', serverAdapter.getRouter());
+
   await app.listen(8080);
 }
 bootstrap();
